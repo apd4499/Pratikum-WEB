7b0601045b3c3dbf2fa3849b2c1653550e7a76f9
Alif Putra Dafianto <72423528+apd4499@users.noreply.github.com>
2020-10-06 10:19:34 +0700

    Add files via upload

---
+++ methodstring.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP</title>
+</head>
+<body>
+
+	<?php
+	$text = " Hai semua";
+	echo str_repeat(str_replace("Hai", "Hallo", $text), 10 );
+	?>
+
+</body>
+</html>
---
+++ tipedata.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP</title>
+</head>
+<body>
+
+	<?php
+	/*
+	berapa baris
+	komentar
+	*/
+
+	$nama	= "Sekolah Koding";
+	$nama2	= 'Bermain Koding';
+
+	echo "Selamat datang di \$nama";
+	?>
+	
+</body>
+</html>
